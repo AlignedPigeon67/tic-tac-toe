9f784efaf98cff09b9918b37d4b0ee183faf4b2c
Tom Hine <tomhine792@gmail.com>
2021-02-17 21:24:25 +0000

    changed formatting

--- app.js
+++ app.js
@@ -1,89 +1,99 @@
 /* gameBoard module */
 const gameBoard = (() => {
-    const board = new Array(9);
+  const board = new Array(9);
 
-    const addToBoard = (marker, arrayIndex) => {
-        if (board[arrayIndex] !== undefined) return;
-        board.splice(arrayIndex, 1, marker);
-        displayController.render(board);
-    };
-    const getBoard = () => { return board };
+  const addToBoard = (marker, arrayIndex) => {
+    if (board[arrayIndex] !== undefined) return;
+    board.splice(arrayIndex, 1, marker);
+    displayController.render(board);
+  };
+  const getBoard = () => {
+    return board;
+  };
 
-    return {addToBoard, getBoard};
+  return { addToBoard, getBoard };
 })();
 
 /* displayController module */
 const displayController = (() => {
-    let dataCells = document.querySelectorAll('[data-cell]');
-    let markerIsX = true;
+  let dataCells = document.querySelectorAll('[data-cell]');
 
-    document.addEventListener('click', e => {
-        const cellNumber = e.target.getAttribute('data-cell');
+  document.addEventListener('click', e => {
+    const cellNumber = e.target.getAttribute('data-cell');
 
-        switch (cellNumber) {
-            case '0':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', );
-                switchMarker();
-                break;
-            case '1':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-            case '2':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-            case '3':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-            case '4':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-            case '5':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-            case '6':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-            case '7':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-            case '8':
-                gameBoard.addToBoard(markerIsX ? 'x' : 'o', cellNumber);
-                switchMarker();
-                break;
-        }
-    });
+    switch (cellNumber) {
+      case '0':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '1':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '2':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '3':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '4':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '5':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '6':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '7':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+      case '8':
+        gameBoard.addToBoard(gameController.isPlayer1Turn() ? 'x' : 'o', cellNumber);
+        gameController.switchPlayerTurn();
+        break;
+    }
+  });
 
-    const render = (inputArray) => {
-        for (let i = 0; i < dataCells.length; i++) {
-            if (inputArray[i] === undefined) continue;
-            dataCells[i].innerHTML = inputArray[i];
-        }
-    };
-    const switchMarker = () => {
-        markerIsX = markerIsX ? false : true;
-    };
+  const render = inputArray => {
+    for (let i = 0; i < dataCells.length; i++) {
+      if (inputArray[i] === undefined) continue;
+      dataCells[i].innerHTML = inputArray[i];
+    }
+  };
 
-    return {render};
+  return { render };
 })();
 
 gameController = (() => {
-    let playerXTurn = true;
+  player1Turn = true;
+
+  const isPlayer1Turn = () => {
+    return player1Turn;
+  };
+
+  const switchPlayerTurn = () => {
+    player1Turn = !player1Turn;
+  };
+
+  return { isPlayer1Turn, switchPlayerTurn };
 })();
 
 /* Player factory */
 const Player = (name, marker) => {
-    let markerCount = 0;
+  let markerCount = 0;
 
-    const getName = () => name;
-    const getMarker = () => marker;
-    const getMarkerCount = () => { return markerCount };
+  const getName = () => name;
+  const getMarker = () => marker;
+  const getMarkerCount = () => {
+    return markerCount;
+  };
 
-    return {getName, getMarker, getMarkerCount};
+  return { getName, getMarker, getMarkerCount };
 };
